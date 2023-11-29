eeaea70c26475cf7f64e6e33b61a6f5944f16838
sunbi8534 <sunbi8534@gmail.com>
2023-11-29 19:23:45 +0900

    git key+++

--- src/main/java/Capstone/server/DTO/ChatGpt/OpenAIRestTemplateConfig.java
+++ src/main/java/Capstone/server/DTO/ChatGpt/OpenAIRestTemplateConfig.java
@@ -4,7 +4,12 @@ import lombok.Value;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.client.RestTemplate;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 @Configuration
 public class OpenAIRestTemplateConfig {
@@ -12,7 +17,16 @@ public class OpenAIRestTemplateConfig {
     @Qualifier("openaiRestTemplate")
     public RestTemplate openaiRestTemplate() {
         RestTemplate restTemplate = new RestTemplate();
-        String openaiApiKey = "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";
+        String s = "";
+        try{
+            ClassPathResource resource = new ClassPathResource("k.txt");
+            BufferedReader br = new BufferedReader(new InputStreamReader(resource.getInputStream()));
+            s = br.readLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        String openaiApiKey = s;
         restTemplate.getInterceptors().add((request, body, execution) -> {
             request.getHeaders().add("Authorization", "Bearer " + openaiApiKey);
             return execution.execute(request, body);
