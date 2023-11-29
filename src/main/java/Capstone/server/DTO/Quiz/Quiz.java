3997794b76034f81653d75ea23a33eebe1a9f6f1
sunbi8534 <sunbi8534@gmail.com>
2023-11-29 20:27:19 +0900

    git key+++++++++

--- src/main/java/Capstone/server/DTO/Quiz/Quiz.java
+++ src/main/java/Capstone/server/DTO/Quiz/Quiz.java
@@ -2,9 +2,13 @@ package Capstone.server.DTO.Quiz;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
+@Setter
 public class Quiz {
     int quizNum;
     String question;
