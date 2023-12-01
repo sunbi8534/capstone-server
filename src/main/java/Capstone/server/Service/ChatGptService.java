d31ad754e168a1d77016246ebeb3e686b766cf46
sunbi8534 <sunbi8534@gmail.com>
2023-12-01 16:35:55 +0900

    to capstone+

--- src/main/java/Capstone/server/Service/ChatGptService.java
+++ src/main/java/Capstone/server/Service/ChatGptService.java
@@ -20,7 +20,7 @@ public class ChatGptService {
 
 
         contents.append(" 를 읽고 " +
-                " 이 문장들에서 얻을 수 있는 개념에 대한 문제와 답을 json형식으로 quizNum변수에 1부터 차례대로 문제번호를 저장하고," +
+                " 이 문장들에서 얻을 수 있는 개념에 대한 문제와 답을 json형식으로 quizNum변수에 int형으로 1부터 차례대로 문제번호를 저장하고," +
                 "question변수에 1개의 문제를, answer변수에 1개의 정답을 저장해서 총 ");
         contents.append(String.valueOf(num));
         contents.append("개 json형식으로 반환해줘. 이때 응답받은 내용을 바로 java의 OpjectMapper로 변환할 거니까 json내용" +
